9f5a1b79671836d7929010de8245c9c67acc863a
Jinyoung lee <codingzombie82@gmail.com>
2020-12-13 13:09:00 +0900

    Updates

---
+++ precache-manifest.70e5fcc273cd0db0f5e8a41d6880a16e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160b9f6d4e8989e64c33f9014593cdfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e96ab629c4c89e50cc89",
+    "url": "/static/css/2.dd33d523.chunk.css"
+  },
+  {
+    "revision": "c81c2fbb2a0fa6d9b07f",
+    "url": "/static/css/main.274db604.chunk.css"
+  },
+  {
+    "revision": "e96ab629c4c89e50cc89",
+    "url": "/static/js/2.4e099f74.chunk.js"
+  },
+  {
+    "revision": "c81c2fbb2a0fa6d9b07f",
+    "url": "/static/js/main.eb5a68f5.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "aa80e7dc23d7c71a0d719433fa1b1f9e",
+    "url": "/static/media/logo2.aa80e7dc.png"
+  }
+]);
